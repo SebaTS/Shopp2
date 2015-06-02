84aa63c9a43580ca6322cd7ad090b1f43fae6d4f
MariuuV <mariue.varela@gmail.com>
2015-06-02 13:10:33 -0300

    Dia 2!!

--- app/src/main/java/com/example/android/shopping/PantallaAsignados.java
+++ app/src/main/java/com/example/android/shopping/PantallaAsignados.java
@@ -1,9 +1,11 @@
 package com.example.android.shopping;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class PantallaAsignados extends ActionBarActivity {
@@ -35,5 +37,9 @@ public class PantallaAsignados extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void verPantallaReportesClick (View v){
+        Intent intent = new Intent (getApplicationContext(), PantallaReportes.class);
+        startActivity (intent);
     }
 }
