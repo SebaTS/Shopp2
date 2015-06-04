1065bee5c12c1cb5ed3952d7478c1908aa6bf3d6
MariuuV <mariue.varela@gmail.com>
2015-06-04 13:43:32 -0300

    Sin background pantalla asignados

--- app/src/main/java/com/example/android/shopping/activity/PantallaReportes.java
+++ app/src/main/java/com/example/android/shopping/activity/PantallaReportes.java
@@ -8,6 +8,8 @@ import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.android.shopping.R;
 
@@ -24,8 +26,8 @@ public class PantallaReportes extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_reportes);
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/android/shopping/db/IndicadoresRepository.java
+++ app/src/main/java/com/example/android/shopping/db/IndicadoresRepository.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 /**
  * Created by android on 04/06/2015.
  */
-public class IndicadoresRepository {
+/*public class IndicadoresRepository {
 
     private ArrayList<Indicador> listaDeIndicadores;
     public IndicadoresRepository(){
 
         Indicador piso
     }
-}
+}*/
