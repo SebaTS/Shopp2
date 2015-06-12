dc68d2cf568ea4e5e91514ab6f99190b2d630b88
Seba <sebastiancruzbossio@gmail.com>
2015-06-12 01:55:12 -0300

    Inicio de programación de PantallaPlanilla

--- app/src/main/java/com/example/android/shopping/activity/PantallaPlanilla.java
+++ app/src/main/java/com/example/android/shopping/activity/PantallaPlanilla.java
@@ -8,23 +8,49 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.example.android.shopping.Entidades.Locacion;
 import com.example.android.shopping.R;
 import com.example.android.shopping.db.LocacionesRepository;
 
+import java.security.KeyStore;
 import java.util.List;
 
 
 public class PantallaPlanilla extends ActionBarActivity {
 
     protected ArrayAdapter<CharSequence> adapter;
+    private Button boton;
+    private Spinner spinner;
+    private ListView listview;
+    private LocacionesRepository locaciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_planilla);
+        this.boton = (Button)findViewById(R.id.btnIniciar);
+        this.spinner = (Spinner)findViewById(R.id.spnFiltro);
+        this.listview = (ListView)findViewById(R.id.lswLocaciones);
+
+        spinner.setEnabled(false);
+        spinner.addView();
+
+        listview.setEnabled(false);
+        this.locaciones = new LocacionesRepository();
+
+        listview.addTouchables(locaciones);
+
+//        adapter = null;
+//       adapter = new ListAdapter();
+//        listview.setAdapter(adapter);
+//        adapter.notifyDataSetChanged();
+
 
 
         //Spinner spinner = (Spinner) findViewById(R.id.spnFiltro);
@@ -41,32 +67,21 @@ public class PantallaPlanilla extends ActionBarActivity {
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_pantalla_asignados, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    public void verPantallaReportesClick (View v){
+    public void verPantallaTomas (View v){
         Intent intent = new Intent (getApplicationContext(), PantallaTomas.class);
         startActivity (intent);
     }
 
+
+    public void IniciarTerminar (View view){
+        if (boton.getText().toString().equals("Iniciar Recorrida")){
+            boton.setText("Terminar Recorrida");
+            spinner.setEnabled(true);
+            listview.setEnabled(true);
+        } else {
+
+        }
+
+    }
 
 }
